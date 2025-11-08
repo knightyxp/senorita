1a2a3250adc898d49661c4febfc96a50d8dea384
Xiangpeng Yang <knightyxp@gmail.com>
2025-11-09 04:30:04 +1100

    models

--- parallel_infer.sh
+++ parallel_infer.sh
@@ -6,7 +6,7 @@ torchrun --nproc_per_node=4 parallel_control_cogvideox_pipeline.py \
   --tasks_json /scratch3/yan204/yxp/VideoX_Fun/data/test_json/4tasks_rem_add_swap_local-style_test.json \
   --output_dir results/senorita_bench_test \
   --model_root /scratch3/yan204/models/cogvideox-5b-i2v \
-  --control_checkpoint /scratch3/yan204/models/senorita-2m/models_half/ff_controlnet_half.pth \
+  --control_checkpoint ./models_half/ff_controlnet_half.pth \
   --seed 0 \
   --num_frames 33 \
   --edited_videos_dir /scratch3/yan204/yxp/VideoX_Fun/results/14b_1-t+1_0_1-t+1_gradual_ground_edit_decouple_decode
